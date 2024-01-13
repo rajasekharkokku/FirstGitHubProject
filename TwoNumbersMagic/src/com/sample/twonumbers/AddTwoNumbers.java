fff6f7b78ed8c9375332770596a83b85614f4b34
Rajasekhara Naidu Kokku <138103223+rajasekharkokku@users.noreply.github.com>
2024-01-14 05:32:31 +0800

    Second commit with subtraction

--- TwoNumbersMagic/src/com/sample/twonumbers/AddTwoNumbers.java
+++ TwoNumbersMagic/src/com/sample/twonumbers/AddTwoNumbers.java
@@ -6,6 +6,8 @@ public class AddTwoNumbers {
 		System.out.println("The sum of two numbers are ---> " +(10+5));
 		
 		System.out.println("Hello GIT-HUB hpw are you");
+		
+		System.out.println("Minus two mnumbers ." +(10-5));
 	}
 
 }
