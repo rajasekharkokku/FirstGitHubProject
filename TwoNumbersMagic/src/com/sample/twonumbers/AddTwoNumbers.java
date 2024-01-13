e2318e3c02d7d1e8a628972d26fb041f337c014d
kraja <kraja@CHERRYMANU>
2024-01-14 04:43:04 +0800

    First commit

---
+++ TwoNumbersMagic/src/com/sample/twonumbers/AddTwoNumbers.java
@@ -0,0 +1,9 @@
+package com.sample.twonumbers;
+
+public class AddTwoNumbers {
+	
+	public static void main(String[] args) {
+		System.out.println("The sum of two numbers are ---> " +(10+5));
+	}
+
+}
