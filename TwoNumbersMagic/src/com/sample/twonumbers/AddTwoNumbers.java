9a72a8720ff2d85368d65dda85dce0691b576be8
Rajasekhara Naidu Kokku <138103223+rajasekharkokku@users.noreply.github.com>
2024-01-14 05:27:54 +0800

    Added hello message

--- TwoNumbersMagic/src/com/sample/twonumbers/AddTwoNumbers.java
+++ TwoNumbersMagic/src/com/sample/twonumbers/AddTwoNumbers.java
@@ -4,6 +4,8 @@ public class AddTwoNumbers {
 	
 	public static void main(String[] args) {
 		System.out.println("The sum of two numbers are ---> " +(10+5));
+		
+		System.out.println("Hello GIT-HUB hpw are you");
 	}
 
 }
